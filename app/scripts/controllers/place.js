b06576a6f5702bd5117ce67721a057efd6f909a6
Michael Krausse <michael.krausse@m-krausse.de>
2014-12-17 20:48:41 +0100

    page for places

---
+++ app/scripts/controllers/place.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name whatamiApp.controller:PlaceCtrl
+ * @description
+ * # PlaceCtrl
+ * Controller of the whatamiApp
+ */
+angular.module('whatamiApp')
+  .controller('PlaceCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
